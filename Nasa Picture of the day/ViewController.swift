20773233895684aec263a38bfc4079efb615501d
David <bolotaev-david@mail.ru>
2019-05-01 19:11:11 +0300

    Initial Commit

---
+++ Nasa Picture of the day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nasa Picture of the day
+//
+//  Created by Давид on 01/05/2019.
+//  Copyright © 2019 Давид. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
